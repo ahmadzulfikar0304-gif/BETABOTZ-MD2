5f2b1c79d434aeb39077e2851c28c1be7103c317
ahmadzulfikar0304-gif <ahmad.zulfikar0304@gmail.com>
2026-04-16 22:32:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.antiporn = true // Auto delete pesan porno (bot harus admin)
 // Prefix
 global.prefix = './#'
 //INI WAJIB DI ISI!//
-global.lann = ''
+global.lann = 'VVIP-69e057552987e6e667fa'
 global.aksesKey = '' // Register dan buy 
 //Daftar terlebih dahulu https://api.betabotz.eu.org
 
